546423576062632a6fce082aed1bb25393a9a37b
Matt Eckert <me@matteckert.com>
2014-03-13 07:11:48 -0500

    Simplify tests

--- test/main.js
+++ test/main.js
@@ -1,155 +1,99 @@
 var should = require('should');
 var flon = require('../lib/flon.js');
 
-describe('flon', function() {
-    it('should allow whitespace everywhere', function() {
-        var flonText = '  hi.there    {     im     [   "an"    "array"  ]    }  hi.what+   "hi"   ';
-        var expected = {"hi": { "there":  { "im" : [ "an", "array" ] }, "what": [ "hi" ] } };
-
-        flon.parse(flonText).should.eql(expected);
+describe('parse', function() {
+    it('should allow extra whitespace everywhere', function() {
+        flon.parse('  a.b+  {  c.d  [  "e"  ]  }  ').should.eql({a: {b: [{c: {d: ['e']}}]}});
     });
 
     describe('value', function() {
         describe('objects', function() {
             it('should handle nested objects', function() {
-                var flonText = 'hi { im "nested" }';
-                var expected = {"hi": { "im" : "nested" } };
-
-                flon.parse(flonText).should.eql(expected);
+                flon.parse('a {b "c"}').should.eql({a: {b: 'c'}});
             });
 
             it('should throw if bad', function() {
-                var flonText = 'this.is { seriously "broken!"';
-
-                flon.parse.bind(null, flonText).should.throw(/^Bad object/);
+                flon.parse.bind(null, 'a { b "c"').should.throw(/^Bad object/);
             });
         });
 
         describe('strings', function() {
             it('should handle simple string values', function() {
-                var flonText = 'hi "there"';
-                var expected = {"hi": "there"};
-
-                flon.parse(flonText).should.eql(expected);
+                flon.parse('a "b"').should.eql({a: 'b'});
             });
 
             it('should handle escaped quotes', function() {
-                var flonText = 'hi "\\"there\\""';
-                var expected = {"hi": '"there"'};
-
-                flon.parse(flonText).should.eql(expected);
+                flon.parse('a "\\"b\\""').should.eql({a: '"b"'});
             });
 
             it('should handle single quoted strings', function() {
-                var flonText = "hi 'there'";
-                var expected = {"hi": "there"};
-
-                flon.parse(flonText).should.eql(expected);
+                flon.parse("a 'b'").should.eql({a: 'b'});
             });
 
             it('should handle multi line strings', function() {
-                var flonText = 'hi { im "a multi\nline string" }';
-                var expected = {"hi": { "im" : "a multi\nline string" } };
-
-                flon.parse(flonText).should.eql(expected);
+                flon.parse('a "b\nc"').should.eql({a: 'b\nc' });
             });
 
             it('should throw if bad', function() {
-                var flonText = 'this "broken';
-
-                flon.parse.bind(null, flonText).should.throw(/^Bad string/);
+                flon.parse.bind(null, 'a "b').should.throw(/^Bad string/);
             });
         });
         
         describe('arrays', function() {
             it('should allow all types', function() {
-                var flonText = 'arrays.can.hold [ "strings" { and "objects" } [ "and" "other" "arrays" ] ]';
-                var expected = { "arrays": { "can": { "hold": [ "strings", { "and": "objects" }, [ "and", "other", "arays" ] ] } } };
+                flon.parse('a [ "" {} [] ]').should.eql({a: ["", {}, []]});
             });
             it('should throw if bad', function() {
-                var flonText = 'this.is [ "broken!"';
-
-                flon.parse.bind(null, flonText).should.throw(/^Bad array/);
+                flon.parse.bind(null, 'a [ "b"').should.throw(/^Bad array/);
             });
         });
     });
     
     describe('keys', function() {
         it('should support underscores', function() {
-            var flonText = '_under____scores__ "rule"';
-            var expected = {"_under____scores__": "rule"};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('_a__b_ "c"').should.eql({_a__b_: 'c'});
         });
 
         it('should handle simple dot notation selectors', function() {
-            var flonText = 'dot.notation.is "cool"';
-            var expected = {"dot":{"notation":{"is":"cool"}}};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a.b.c "d"').should.eql({a: {b: {c: 'd'}}});
         });
 
         it('should handle more than one dot notation selector', function() {
-            var flonText = 'dot.notation.is "cool" dot.notation.grows.on "you"';
-            var expected = {"dot":{"notation":{"is":"cool","grows":{"on":"you"}}}};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a.b "c" a.d "e"').should.eql({a: {b: 'c', d: 'e'}});
         });
         
         it('should handle array append notation', function() {
-            var flonText = 'append.here+ "nice"';
-            var expected = {"append":{"here":["nice"]}};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a.b+ "c"').should.eql({a: {b: ['c']}});
         });
 
         it('should handle appending to an array that already exists', function() {
-            var flonText = 'append { here ["this" "is" "very"] } append.here+ "nice"';
-            var expected = {"append":{"here":["this","is","very","nice"]}};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a ["b"] a+ "c"').should.eql({a: ['b', 'c']});
         });
 
         it('should throw if bad', function() {
-            var flonText = '"porkbutt"';
-
-            flon.parse.bind(null, flonText).should.throw(/^Bad key/);
+            flon.parse.bind(null, '"a"').should.throw(/^Bad key/);
         });
     });
 
     describe('comments', function() {
         it('should handle single line comments', function() {
-            var flonText = 'hi "there" // all the way to the end\nnot "here"';
-            var expected = {"hi": "there", "not": "here"};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a "b" // c\nd "e"').should.eql({a: 'b', d: 'e'});
         });
 
         it('should handle multi line comments', function() {
-            var flonText = '/* beginning */ hi /* between */ "there" /* end */';
-            var expected = {"hi": "there"};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('/* a */ b /* c */ "d" /* e */').should.eql({b: 'd'});
         });
 
-        it('should ignore single line comments in multi line comments', function() {
-            var flonText = 'hi "there" /* // all the way to the end */ not "here"';
-            var expected = {"hi": "there", "not": "here"};
-
-            flon.parse(flonText).should.eql(expected);
+        it('should ignore comments inside comments', function() {
+            flon.parse('a "b" /* // c */ d "e"').should.eql({a: 'b', d: 'e'});
         });
 
         it('should ignore comments in strings', function() {
-            var flonText = 'hi "/* comment */ there"';
-            var expected = {"hi": "/* comment */ there"};
-
-            flon.parse(flonText).should.eql(expected);
+            flon.parse('a "/* b */ c"').should.eql({a: '/* b */ c'});
         });
 
         it('should throw if bad', function() {
-            var flonText = 'hi "there" /* // all the way to the end *// not "here"';
-
-            flon.parse.bind(null, flonText).should.throw(/^Bad comment/);
+            flon.parse.bind(null, 'a "b" /* // c *// d "e"').should.throw(/^Bad comment/);
         });
-    });   
+    });
 });
